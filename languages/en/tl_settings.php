3f6e3716da3e838eb298c02b81ab53aeddd6c1cf
Christoph Wiechert <wio@psitrax.de>
2012-03-05 13:04:44 +0100

    Added: english translation

---
+++ languages/en/tl_settings.php
@@ -0,0 +1,14 @@
+<?php if(!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ *
+ * PHP version 5
+ * @copyright  4ward.media 2011
+ * @author     Christoph Wiechert <christoph.wiechert@4wardmedia.de>
+ * @package    filepicker4ward
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['tl_settings']['filepicker_replaceFileTree'] = array('Use filepicker for choosing files','Use the filepicker to choose images and files.');
+
+?>
